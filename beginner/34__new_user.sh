25db3c4f5ebe2e48225d4a96de2e05ebe2e725a3
omiete01 <estheromiete01@gmail.com>
2025-07-27 16:01:51 +0100

    working with multiples users

--- beginner/34__new_user.sh
+++ beginner/34__new_user.sh
@@ -11,26 +11,26 @@ fi
 users=$1
 
 # while IFS= read -r user; do
-# for user in $(cat "$users"); do
-groupadd $user
-useradd -m -g $user $user
-if [[ $? -eq 0 ]]; then
-# passwd $user $user123
-# user_home=/home/$user
-    ssh_dir=/home/$user/.ssh
-    auth_key=/home/$user/.ssh/authorized_keys
+for user in $(cat "$users"); do
+    groupadd $user
+    useradd -m -g $user $user
+    if [[ $? -eq 0 ]]; then
+    # passwd $user $user123
+    # user_home=/home/$user
+        ssh_dir=/home/$user/.ssh
+        auth_key=/home/$user/.ssh/authorized_keys
 
-    # mkdir $user_home
-    # chmod 700 $user_home
+        # mkdir $user_home
+        # chmod 700 $user_home
 
-    mkdir $ssh_dir
-    chmod 700 $ssh_dir
-    touch $auth_key
-    chmod 600 $auth_key
-    chown -R $user:$user /home/$user
-else
-    echo "Something went wrong."
-    exit 1
+        mkdir $ssh_dir
+        chmod 700 $ssh_dir
+        touch $auth_key
+        chmod 600 $auth_key
+        chown -R $user:$user /home/$user
+    else
+        echo "Something went wrong."
+        exit 1
 
-fi
-# done
+    fi
+done
